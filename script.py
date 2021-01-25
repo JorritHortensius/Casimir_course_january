dd3865b8fe1e28c62b6448886c74b1771ebf9374
Jorrit <j.r.hortensius@tudelft.nl>
2021-01-25 13:52:43 +0100

    Working test implemented

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
-print('hello allemaal')
+import test
+
+radius = 3
+cf = test.circumference_circle(radius)
+area = test.area_circle(radius)
+
+print('radius is', radius)
+print('circumference is', cf)
+print('area is', area)
